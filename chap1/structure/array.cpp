d983649782c0282bd3e08f9f9deb94f062d174f3
Kingmintchoco <jrkr7@naver.com>
2022-03-16 12:34:57 +0900

    2022 03 16 WED

--- chap1/structure/array.cpp
+++ chap1/structure/array.cpp
@@ -3,5 +3,28 @@
 using namespace std;
 
 int main(){
-    
+    // backjoon 숫자의 개수
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    int cnt[10] = {
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+    };
+
+    int result = a * b * c;
+
+    while(result != 0){
+        int tmp = result % 10;
+
+        for(int i = 0; i < 10; ++i){
+            if(tmp == i){
+                cnt[i]++;
+            }
+        }
+        result /= 10;
+    }
+
+    for(int i = 0; i < 10; ++i){
+        cout << cnt[i] << "\n";
+    }
 }
